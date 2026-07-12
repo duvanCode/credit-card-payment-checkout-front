162ff0d61d9b54128c00aa8bfee8ec66876eb35e
DuvanCode <duvanyepezfa@gmail.com>
2026-07-11 20:35:49 -0500

    chore(init): bootstrap app React Native y tooling base

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
+};
